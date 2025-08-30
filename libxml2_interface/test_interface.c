e8660b8c477844f79657f20e06cae9fa2387db2d
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-08-30 11:43:38 +0100

    Added C interface to libxml2

---
+++ libxml2_interface/test_interface.c
@@ -0,0 +1,5 @@
+#include "wrapper.h"
+
+int main() {
+    return 0;
+}
---
+++ libxml2_interface/wrapper.h
@@ -0,0 +1,14 @@
+#ifndef LIBXML_WRAPPER_H
+#define LIBXML_WRAPPER_H
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+
+#ifdef _cplusplus
+}
+#endif
+#endif
