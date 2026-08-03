cd5551297d4fca2c54f146659796975ad23552d6
Z M Will <z.m.williams4@gmail.com>
2026-08-03 18:01:39 +0000

    Updated build script

--- build.rs
+++ build.rs
@@ -2,28 +2,56 @@ use cmake::Config;
 use std::env;
 use std::path::{Path, PathBuf};
 
+fn fetch_lib_path() -> Option<String> {
+    if let Ok(lib_path) = env::var("LIBXML2_INCLUDE_DIR") {
+        if Path::new(&lib_path).exists() {
+            println!("cargo:warning=Using LIBXML2_INCLUDE_DIR= {}", lib_path);
+            return Some(lib_path.to_string());
+        }
+    }
+
+
+    let lib_path = "/usr/include/libxml2";
+    if Path::new(lib_path).exists() {
+        return Some(lib_path.to_string())
+    } 
+
+    None
+}
+
+fn generate_config() -> Config {
+    let mut config = Config::new("libxml2_interface");
+
+    if let Ok(cmake_toolchain_file) = env::var("CMAKE_TOOLCHAIN_FILE") {
+        config.define("CMAKE_TOOLCHAIN_FILE", cmake_toolchain_file);
+    }
+
+    if let Ok(out_dir) = env::var("OUT_DIR") {
+        config.out_dir(out_dir);
+    }
+
+    config
+}
+
 fn main() {
-    let path = Config::new("libxml2_interface")
-        .out_dir(env::var("OUT_DIR").unwrap())
-        .build();
+    let path = generate_config().build();
 
     println!("cargo:rustc-link-search=native={:?}", path);
     println!("cargo:rustc-link-lib=xml2");
 
+    let callbacks = bindgen::CargoCallbacks::new();
+
     let builder = bindgen::builder()
         .header("libxml2_interface/wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
+        .parse_callbacks(Box::new(callbacks));
 
-    let bindings: bindgen::Bindings;
-    let lib_path = "/usr/include/libxml2";
-    if Path::new(lib_path).exists() {
-        bindings = builder
-            .clang_arg(format!("-I{}", lib_path))
-            .generate()
-            .expect("Unable to generate bindings");
-    } else {
-        bindings = builder.generate().expect("Unable to generate bindings");
+    let bindings = match fetch_lib_path() {
+        Some(lib_path) => builder.clang_arg(format!("-I{}", lib_path)).generate(),
+        None => builder.generate(),
     }
+    .expect("Unable to generate bindings");
+
+    println!("cargo:warning=Bindings-generated");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
